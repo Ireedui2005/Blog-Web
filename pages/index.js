d635ba62fa889a79c3393e69672fb275144fb888
odbaatar tulgaa <metatulgaa@gmail.com>
2024-08-12 22:37:47 +0800

    fix: import error

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { AllPost } from "@/Components/AllPost";
-import { Banner } from "@/Components/Banner";
-import { TrendingList } from "@/Components/treding/TrendingList";
-
+import { AllPost } from "../components/AllPost";
+import { Banner } from "../components/Banner";
+import { TrendingList } from "../components/treding/TrendingList";
 
 export default function Home() {
   return (
